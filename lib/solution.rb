80ef58f5dd191c84b67b20c2564f25f8c45375f2
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-22 11:06:15 +0100

    upd

--- lib/solution.rb
+++ lib/solution.rb
@@ -4,19 +4,26 @@ def clock_angle(time)
 
     hands = time.split(":")
 
-    minute_hand_angle = hands[1].to_i * 6
-    hour_hand_angle = (hands[0].to_i+ (hands[1].to_i / 60.0 ) ) * 30
+    # minute_hand_angle = hands[1].to_i * 6
+    # hour_hand_angle = (hands[0].to_i+ (hands[1].to_i / 60.0 ) ) * 30
 
-    angle = hour_hand_angle - minute_hand_angle
+    # angle = hour_hand_angle - minute_hand_angle
 
-    if angle > 180
+    # if angle > 180
 
-        (angle - 360).abs
+    #     (angle - 360).abs
         
-    else
+    # else
 
-        angle
+    #     angle
         
-    end
+    # end
+
+    (30 * hands[0].to_i - 5.5 * hands[1].to_i ).abs
 
 end
+
+var = clock_angle("5:43")
+
+binding.pry
+0
